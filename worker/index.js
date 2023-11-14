d7fc08de47ed64295b873e5402aa3ea7e062c5d4
EaswarDSharma <easwardsharma@gmail.com>
2023-11-14 18:47:26 +0530

    string cnvrt

--- worker/index.js
+++ worker/index.js
@@ -12,13 +12,13 @@ const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
-  return String(fib(index - 1) + fib(index - 2));
+  return fib(index - 1) + fib(index - 2);
   
 }
 
 sub.on('message', async (channel, message) => {
   //redisClient.hset('values', message,await fibb(parseInt(message))
-  const foodmess= fib(message)
+  const foodmess= String(fib(message))
   await redisClient.hset('values', message,foodmess)
 })
 sub.subscribe('insert');
