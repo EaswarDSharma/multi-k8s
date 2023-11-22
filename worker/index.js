6d1b3705c1543ca063cec7bf08a520f76c1a3925
EaswarDSharma <easwardsharma@gmail.com>
2023-11-22 17:33:53 +0530

    fib to food

--- worker/index.js
+++ worker/index.js
@@ -15,10 +15,20 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
   
 }
+async function edfood(query){
+  url="temporary";
+   await axios.get(`https://api.edamam.com/search?q=${query}&app_id=ac3ed5c4&app_key=32175cda1f892ab16589951f6d022100&from=0&to=1`)
+  .then((response) => {
+    url=response.data.hits[0].recipe.uri;
+  })
+  .catch((error) => {
+  url="not found"});
+  return url;
+ }
 
 sub.on('message', async (channel, message) => {
   //redisClient.hset('values', message,await fibb(parseInt(message))
-  const foodmess= String(fib(message))
+  const foodmess= edfood(message)
   await redisClient.hset('values', message,foodmess)
 })
 sub.subscribe('insert');
